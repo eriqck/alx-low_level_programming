4b808c8971a71743cb29fe9352a29b95fab4c0e8
eriqck <ericdavid348@gmail.com>
2022-03-16 08:53:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print lower case a-z
+ * Return: Return
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
