623d6d2f5c7168e9f600ecc0bcb58d69ea5e0e5d
eriqck <ericdavid348@gmail.com>
2022-03-16 07:59:08 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#include <stdio.h>
-/**
- * main - Entry point
- * Description: print _putchar
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	putchar("_putchar\n");
-	return (0);
-}
+#define MAIN_H
