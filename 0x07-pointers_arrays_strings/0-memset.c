33a2b8e4c8b0cc3b8927bc23852a0053b54270ab
eriqck <ericdavid348@gmail.com>
2022-03-29 04:36:20 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	/ Delacring FOR /
 	for (i = 0; i < n; i++)
 	{
-		(s + i) = b; /*add 1 position s*/
+		*(s + i) = b;
 
 	}
 
