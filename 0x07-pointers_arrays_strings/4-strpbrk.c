43489208370bf2d3f3f0f921386503f8b648662c
eriqck <ericdavid348@gmail.com>
2022-03-29 06:17:13 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strpbk - searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
  * @accept: string to check against
  *
  * Return: pointer to byte in s that matches or NULL if no match
  */
-char *_strpbk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
