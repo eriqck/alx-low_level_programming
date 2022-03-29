79e22d0279465684ba8131d988c73dea1f0c7729
eriqck <ericdavid348@gmail.com>
2022-03-29 07:21:55 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,11 +6,12 @@
  *
  * Return: number of bytes of s in accept
  */
-unsigned int *_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
 	for (i = 0; s[i]; i++)
+	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
@@ -18,6 +19,6 @@ unsigned int *_strspn(char *s, char *accept)
 		}
 	if (!accept[j])
 		break;
-
+	}
+	return (i);
 }
-return (i);
