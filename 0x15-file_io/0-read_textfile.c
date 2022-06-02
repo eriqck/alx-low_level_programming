a699ca0496e2f88a1e22c9a80be927c82de1d730
eriqck <ericdavid348@gmail.com>
2022-06-02 05:40:29 -0700

    read_text_files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,37 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, fread, fwrite;
-	char *totalSize;
-
-	totalSize = malloc(sizeof(char) * letters);
-	if (totalSize == NULL)
-	return (0);
-	if (filename == NULL)
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
 		return (0);
-
-	file = open(filename, 0_RDONLY);
-	if (file == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-	fread = read(file, totalSize, letters);
-	if (fread == -1)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-	fwrite = write(STDOUT_FILENO, totalSize, fread);
-	if (write == -1)
-	return (0);
-	close(file);
-	free(totalSize);
-	return (fwrite);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
